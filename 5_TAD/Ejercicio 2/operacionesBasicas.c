aeefbddda6f5f51106e2f7cc71698822d9b6e2a5
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-18 15:49:08 -0300

    operaciones basicas updated

--- 5_TAD/Ejercicio 2/operacionesBasicas.c
+++ 5_TAD/Ejercicio 2/operacionesBasicas.c
@@ -1,15 +1,15 @@
-/*Luego desarrolle un programa en “C” que a partir del ingreso de dos variables reales y el operador 
-(+: suma, -: resta, *: multiplicación, /: división), 
-invoque al TDA y muestre los resultados de la operación seleccionada.
+/*Luego desarrolle un programa en ï¿½Cï¿½ que a partir del ingreso de dos variables reales y el operador
+(+: suma, -: resta, *: multiplicaciï¿½n, /: divisiï¿½n),
+invoque al TDA y muestre los resultados de la operaciï¿½n seleccionada.
 
-OBJETIVO: 
+OBJETIVO:
 	robarme una mina ajena
 ESTRATEGIA:
 implementar un menu que se seleccione, muestre e invoque las funciones con los calculos
 */
-#include<stdio.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
 #include "operacionesBasicas.h"
 
 void ingresarDatos();
@@ -19,80 +19,84 @@ void mostrarMenu();
 
 float n1, n2;
 
-int main(){
+int main()
+{
 	iniciarPrograma();
 	return 0;
 }
 
-void ingresarDatos(){
-	printf("\nDigite un número para la primer variable: ");
+void ingresarDatos()
+{
+	printf("\nDigite un nï¿½mero para la primer variable: ");
 	scanf("%f", &n1);
-	printf("\nDigite un número para la segunda variable: ");
+	printf("\nDigite un nï¿½mero para la segunda variable: ");
 	scanf("%f", &n2);
-	
+
 	suma(n1, n2);
 	resta(n1, n2);
 	multiplicacion(n1, n2);
 	division(n1, n2);
-	
 }
 
-void iniciarPrograma(){
+void iniciarPrograma()
+{
 	setlocale(LC_ALL, "spanish");
 	mostrarMenu();
 }
 
-void menu(){
+void menu()
+{
 	printf("\t\tMENU");
-	printf("\n1.Ingresar números para las variables.\n2.Calcular suma.\n3.Calcular resta.\n4.Calcular multiplicación.\n5.Calcular división.\n6.Salir del menú");
-	printf("\n\nDigite una opción: ");
+	printf("\n1.Ingresar nï¿½meros para las variables.\n2.Calcular suma.\n3.Calcular resta.\n4.Calcular multiplicaciï¿½n.\n5.Calcular divisiï¿½n.\n6.Salir del menï¿½");
+	printf("\n\nDigite una opciï¿½n: ");
 }
 
-void mostrarMenu(){
-	int opcion=0;
-	do{
+void mostrarMenu()
+{
+	int opcion = 0;
+	do
+	{
 		menu();
 		scanf("%d", &opcion);
-		
-		switch(opcion){
-			case 1:
-		    		ingresarDatos();
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			case 2:
-					printf("\nResultado: %.2f", suma(n1, n2));
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			case 3:
-					printf("\nResultado: %.2f", resta(n1, n2));
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			case 4:
-					printf("\nResultado: %.2f", multiplicacion(n1, n2));
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			case 5:
-					printf("\nResultado: %.2f", division(n1, n2));
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			case 6:
-					printf("\n\t\t---SALIENDO DEL MENU---");
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;
-			default:
-					printf("\n\t\tOPCION INCORRECTA. DIGITE SOLO LAS OPCIONES DADAS EN EL MENU");
-		    		printf("\n\n");
-		    		system("pause");
-		    		break;		
+
+		switch (opcion)
+		{
+		case 1:
+			ingresarDatos();
+			printf("\n\n");
+			system("pause");
+			break;
+		case 2:
+			printf("\nResultado: %.2f", suma(n1, n2));
+			printf("\n\n");
+			system("pause");
+			break;
+		case 3:
+			printf("\nResultado: %.2f", resta(n1, n2));
+			printf("\n\n");
+			system("pause");
+			break;
+		case 4:
+			printf("\nResultado: %.2f", multiplicacion(n1, n2));
+			printf("\n\n");
+			system("pause");
+			break;
+		case 5:
+			printf("\nResultado: %.2f", division(n1, n2));
+			printf("\n\n");
+			system("pause");
+			break;
+		case 6:
+			printf("\n\t\t---SALIENDO DEL MENU---");
+			printf("\n\n");
+			system("pause");
+			break;
+		default:
+			printf("\n\t\tOPCION INCORRECTA. DIGITE SOLO LAS OPCIONES DADAS EN EL MENU");
+			printf("\n\n");
+			system("pause");
+			break;
 		}
 		system("cls");
-	}while(opcion != 6);
+	} while (opcion != 6);
 }
-
-
