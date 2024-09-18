3915e75a3678d074ce6d68d7f100e66f3b52700f
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-18 15:49:24 -0300

    dado updated

--- 5_TAD/Ejercicio 4/Dado.h
+++ 5_TAD/Ejercicio 4/Dado.h
@@ -1,13 +1,12 @@
-/*Desarrollar un TDA “Dado” que permita representar un dado de seis caras, 
-en donde los valores de las caras son números enteros. 
+/*Desarrollar un TDA ï¿½Dadoï¿½ que permita representar un dado de seis caras,
+en donde los valores de las caras son nï¿½meros enteros.
 
 Las operaciones disponibles deben ser:
 
 a) inicializar valores de las caras
 
-b) lanzar dado: que simule el lanzamiento del lado y devuelva un valor random 
+b) lanzar dado: que simule el lanzamiento del lado y devuelva un valor random
 dentro del dominio de los valores de las caras
 
 c) valor actual: que retorne el valor de la cara actual, luego de un lanzamiento
 */
-
--- 5_TAD/Ejercicio 5/pilaEstatica.c
+++ 5_TAD/Ejercicio 5/pilaEstatica.c
@@ -1,81 +1,84 @@
-/*Escribir un TDA para cargar y mostrar los elementos de una pila estática de elementos 
+/*Escribir un TDA para cargar y mostrar los elementos de una pila estï¿½tica de elementos
 de tipo cadena de caracteres. Luego desarrolle un programa para probar dicho TDA.*/
 
-
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
-#include<stdlib.h>
-#include<locale.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <locale.h>
 #include "pilaEstatica.h"
-
 
 void iniciarPrograma();
 void menu();
 void mostrarMenu();
 void ingresarDatos();
 
-int main(){
+int main()
+{
 	iniciarPrograma();
 	return 0;
 }
 
-void iniciarPrograma(){
+void iniciarPrograma()
+{
 	setlocale(LC_ALL, "spanish");
 	crearPila();
 	mostrarMenu();
 }
 
-void menu(){
+void menu()
+{
 	printf("\t\tMENU");
-	printf("\n1.Cargar elementos.\n2.Mostrar elementos.\n3.Salir del menú.\n\nDigite una opción: ");
+	printf("\n1.Cargar elementos.\n2.Mostrar elementos.\n3.Salir del menï¿½.\n\nDigite una opciï¿½n: ");
 }
 
-void mostrarMenu(){
-	int opc=0;
-	do{
+void mostrarMenu()
+{
+	int opc = 0;
+	do
+	{
 		menu();
 		scanf("%d", &opc);
-		
-		switch(opc){
-			case 1:
-					ingresarDatos();
-					printf("\n\n");
-					system("pause");
-					break;
-			case 2:
-					mostrarElementos(pila);
-					printf("\n\n");
-					system("pause");
-					break;
-			case 3:
-					printf("\n\t\t---SALIENDO DEL MENÚ---");
-				    printf("\n.....____________________ , ,__");
-				    printf("\n....../ `---___________----_____|] - - - - - - - - ¦ ¦¦¦¦D ");
-				    printf("\n...../_==o;;;;;;;;_______.:/");
-				    printf("\n.....), ---.(_(__) /");
-				    printf("\n....// (..) ), ----");
-				    printf("\n...//___//");
-				    printf("\n..//___//");
-				    printf("\n.//___//");
-					printf("\n\n");
-					system("pause");
-					break;					
-			default:
-				    printf("\nOPCIÓN INCORRECTA. DIGITE SOLO LAS OPCIONES QUE SE MUESTRAN EN EL MENÚ");
-					printf("\n\n");
-					system("pause");
-					break;			   
+
+		switch (opc)
+		{
+		case 1:
+			ingresarDatos();
+			printf("\n\n");
+			system("pause");
+			break;
+		case 2:
+			mostrarElementos(pila);
+			printf("\n\n");
+			system("pause");
+			break;
+		case 3:
+			printf("\n\t\t---SALIENDO DEL MENï¿½---");
+			printf("\n.....____________________ , ,__");
+			printf("\n....../ `---___________----_____|] - - - - - - - - ï¿½ ï¿½ï¿½ï¿½ï¿½D ");
+			printf("\n...../_==o;;;;;;;;_______.:/");
+			printf("\n.....), ---.(_(__) /");
+			printf("\n....// (..) ), ----");
+			printf("\n...//___//");
+			printf("\n..//___//");
+			printf("\n.//___//");
+			printf("\n\n");
+			system("pause");
+			break;
+		default:
+			printf("\nOPCIï¿½N INCORRECTA. DIGITE SOLO LAS OPCIONES QUE SE MUESTRAN EN EL MENï¿½");
+			printf("\n\n");
+			system("pause");
+			break;
 		}
 		system("cls");
-	}while(opc != 3);
+	} while (opc != 3);
 }
-void ingresarDatos(){
+void ingresarDatos()
+{
 	char[80] palabra;
 	printf("\nDigite una palabra cualquiera: ");
 	fflush(stdin);
 	scanf("%[^\n]s", &palabra);
 	apilar(palabra);
 }
-
-
