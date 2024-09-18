960a6008f649afeadaa51501effdade6ad3b9bd9
JoseEnriquez88 <eenriquez.jose@gmail.com>
2024-09-18 15:47:06 -0300

    demo updated

--- 1_pilas_y_colas_con_arreglos/demo.c
+++ 1_pilas_y_colas_con_arreglos/demo.c
@@ -4,10 +4,11 @@
 
 typedef int tArrayEnteros[MAX];
 
-typedef struct{
+typedef struct
+{
   tArrayEnteros listaEnteros;
   int tope;
-}tPila;
+} tPila;
 
 tPila pila;
 
@@ -22,16 +23,18 @@ void desapilar();
 int cima(tPila);
 void visualizarElementos(tPila);
 
-int main(){
+int main()
+{
   iniciarPrograma();
   return 0;
 }
 
-void iniciarPrograma(){
+void iniciarPrograma()
+{
   crearPilaVacia();
-  printf("Pila vacía %s\n", pilaVacia(pila) ? "si": "no");
+  printf("Pila vacía %s\n", pilaVacia(pila) ? "si" : "no");
   apilar(1);
-  printf("Pila vacía %s\n", pilaVacia(pila) ? "si": "no");
+  printf("Pila vacía %s\n", pilaVacia(pila) ? "si" : "no");
   apilar(2);
   apilar(3);
   apilar(4);
@@ -39,53 +42,66 @@ void iniciarPrograma(){
   desapilar();
   visualizarElementos(pila);
   printf("Elemento en cima %d\n", cima(pila));
-
 };
 
-
-void crearPilaVacia(){
+void crearPilaVacia()
+{
   pila.tope = -1;
 };
 
-bool pilaVacia(tPila pPila){
-  if(pPila.tope == -1) return true;
+bool pilaVacia(tPila pPila)
+{
+  if (pPila.tope == -1)
+    return true;
   return false;
 };
 
-bool pillaLlena(tPila pPila){
-  if(pPila.tope == (MAX -1)) return true;
+bool pillaLlena(tPila pPila)
+{
+  if (pPila.tope == (MAX - 1))
+    return true;
   return false;
 };
 
-void apilar(int pElemento){
-  if(pillaLlena(pila) != true){
+void apilar(int pElemento)
+{
+  if (pillaLlena(pila) != true)
+  {
     pila.tope = pila.tope + 1;
     pila.listaEnteros[pila.tope] = pElemento;
     printf("Elemento insertado: %d\n", pila.listaEnteros[pila.tope]);
-  }else{
+  }
+  else
+  {
     printf("Pila llena!\n");
   }
 };
 
-void desapilar(){
-  if(pilaVacia(pila) == true){
+void desapilar()
+{
+  if (pilaVacia(pila) == true)
+  {
     printf("Pila vacía");
-  }else{
+  }
+  else
+  {
     pila.listaEnteros[pila.tope] = 0;
     pila.tope = pila.tope - 1;
     printf("Se quitó un elemento \n");
   }
 };
 
-int cima(tPila pPila){
+int cima(tPila pPila)
+{
   return pPila.listaEnteros[pPila.tope];
 };
 
-void visualizarElementos(tPila pPila){
+void visualizarElementos(tPila pPila)
+{
   int i;
   printf("Elementos en la pila: \n");
-  for(i=0;i<=pPila.tope;i++){
+  for (i = 0; i <= pPila.tope; i++)
+  {
     printf("%d\n", pPila.listaEnteros[i]);
-
   }
 };
